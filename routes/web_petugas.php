fc697a00faa698970434f252cac14090044b5453
Mister K <khidirgaminghd@gmail.com>
2026-02-16 19:41:46 +0700

    add route petugas

---
+++ routes/web_petugas.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Petugas\DashboardController;
+use App\Http\Controllers\Petugas\ParkirController;
+use App\Http\Controllers\Petugas\TransaksiController;
+
+Route::prefix('petugas')
+    ->middleware(['auth', 'petugas'])
+    ->name('petugas.')
+    ->group(function () {
+
+        // ================= DASHBOARD =================
+        Route::get('/dashboard', [DashboardController::class, 'index'])
+            ->name('dashboard');
+
+        // ================= STRUK PARKIR =================
+        Route::get('/parkir/{id}/struk', [ParkirController::class, 'struk'])
+            ->name('parkir.struk');
+        
+        //=================== Transaksi =================
+        Route::get('/transaksi', [TransaksiController::class, 'index'])
+            ->name('transaksi.index');
+        Route::get('/transaksi/create', [TransaksiController::class, 'create'])
+            ->name('transaksi.create');
+        Route::post('/transaksi', [TransaksiController::class, 'store'])
+            ->name('transaksi.store');
+        Route::put('/transaksi/{transaksi}/keluar', [TransaksiController::class, 'keluar'])
+            ->name('transaksi.keluar');
+    });
+
+?>
