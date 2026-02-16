4d7753d1f48ec238a7dd9781c5910a69481d833e
Mister K <khidirgaminghd@gmail.com>
2026-02-16 18:41:01 +0700

    add some data in tabel transaksi

--- app/Models/Transaksi.php
+++ app/Models/Transaksi.php
@@ -3,8 +3,38 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\AreaParkir;
+use App\Models\Tarif;
 
 class Transaksi extends Model
 {
-    //
+    protected $table = 'transaksi';
+    protected $fillable = [
+        'id_kendaraan',
+        'waktu_masuk',
+        'waktu_keluar',
+        'id_tarif',
+        'durasi_jam',
+        'biaya_total',
+        'status',
+        'user_id',
+        'id_area',
+    ];
+
+    // Relasi dengan User, AreaParkir, dan Tarif
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function area()
+    {
+        return $this->belongsTo(AreaParkir::class, 'id_area');
+    }
+
+    public function tarif()
+    {
+        return $this->belongsTo(Tarif::class, 'id_tarif');
+    }
 }
